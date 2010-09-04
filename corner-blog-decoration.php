0e83f4a2eb234694ed3ab14b68811177e04c5999
noeprivacy <noeprivacy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-04 15:38:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/corner-blog-decoration/trunk@284981 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ corner-blog-decoration.php
@@ -0,0 +1,138 @@
+<?php
+/*
+Plugin Name: Corner Blog Decoration
+Plugin URI: http://www.noeonline.info/
+Description: This plugin will create an decoration on your blog at the corner of user browser like a blogspot blog. You can customize these decoration. Let's check the screenshot for an example. The decoration menu can be found on <a href="tools.php?page=corner-blog-decoration.php">Tools > Blog Decoration</a>.
+Author: Imanuel Novian (nick: noe, wp: noeprivacy)
+Version: 1.0
+Author URI: http://www.noeonline.info/
+*/
+
+function cbd_admin() {
+add_submenu_page( 'tools.php', 'Corner Blog Decoration', 'Blog Decoration', 10, __FILE__, 'cbd_admin_menu' );
+}
+
+function cbd_first() {
+if ( get_option( 'cbd_str' ) == false ) {
+add_option( 'cbd_str', '<a href="http://www.google.com/"><img src="http://www.google.com/intl/en_com/images/srpr/logo1w.png" alt="cbd" /></a>' );
+}
+if ( get_option( 'cbd_left_right' ) == false ) {
+add_option( 'cbd_left_right', 'right' );
+}
+if ( get_option( 'cbd_top_bottom' ) == false ) {
+add_option( 'cbd_top_bottom', 'bottom' );
+}
+if ( get_option( 'cbd_left_right_px' ) == false ) {
+add_option( 'cbd_left_right_px', '5' );
+}
+if ( get_option( 'cbd_top_bottom_px' ) == false ) {
+add_option( 'cbd_top_bottom_px', '5' );
+}
+}
+
+function cbd_admin_menu() {
+if ( ( get_option( 'cbd_str' ) && get_option( 'cbd_left_right' ) && get_option( 'cbd_top_bottom' )
+ && get_option( 'cbd_left_right_px' ) && get_option( 'cbd_top_bottom_px' ) ) == false ) {
+cbd_first();
+}
+
+echo('
+<div class="wrap">
+<h2>Corner Blog Decoration Options</h2>
+');
+if ( isset( $_POST['submit'] ) ) {
+$str = strtr( $_POST['cbd_str'], array( '"' => '&#34;', '\\' => '', '\'' => '&#39;' ) );
+$cbd = update_option( 'cbd_str', $str );
+$cbd .= update_option( 'cbd_left_right', $_POST['cbd_left_right'] );
+$cbd .= update_option( 'cbd_top_bottom', $_POST['cbd_top_bottom'] );
+$cbd .= update_option( 'cbd_left_right_px', $_POST['cbd_left_right_px'] );
+$cbd .= update_option( 'cbd_top_bottom_px', $_POST['cbd_top_bottom_px'] );
+if ( $cbd ) echo('<div class="updated"><p><strong>Your setting has been save.</strong></p></div>');
+else echo('<div class="error"><p><strong>Your setting not save.</strong></p></div>');
+}
+
+echo('
+<form action="" method="post">
+<table class="form-table">
+<tr><td>HTML to insert<br/><span class="cbd_admin_hint">(Insert "&lt;a&gt;" tag, "&lt;img&gt;" tag, or javascript)<br/></span></td><td><textarea name="cbd_str" rows="5" cols="50">' . get_option( 'cbd_str' ) . '</textarea></td></tr>
+<tr><td>Horizontal Position<br/><span class="cbd_admin_hint">("Left" or "Right")</span></td><td><select name="cbd_left_right">
+');
+switch( get_option( 'cbd_left_right' ) ) {
+case 'left' :
+echo('<option value="left" selected="true">Left</option>
+<option value="right">Right</option>');
+break;
+case 'right' :
+echo('<option value="left">Left</option>
+<option value="right" selected="true">Right</option>');
+break;
+}
+echo('</select>&nbsp;<input type="text" name="cbd_left_right_px" value="' . get_option( 'cbd_left_right_px' ) . '" size="4" />&nbsp;px</td></tr>
+<tr><td>Vertical Position<br/><span class="cbd_admin_hint">("Top" or "Bottom")</span></td><td><select name="cbd_top_bottom">
+');
+switch( get_option( 'cbd_top_bottom' ) ) {
+case 'top' :
+echo('<option value="top" selected="true">Top</option>
+<option value="bottom">Bottom</option>');
+break;
+case 'bottom' :
+echo('<option value="top">Top</option>
+<option value="bottom" selected="true">Bottom</option>');
+break;
+}
+echo('</select>&nbsp;<input type="text" name="cbd_top_bottom_px" value="' . get_option( 'cbd_top_bottom_px' ) . '" size="4" />&nbsp;px</td></tr>
+<tr><td><input class="button-primary" type="submit" name="submit" value="Save Setting" /></td><td>&nbsp;</td></tr>
+</table>
+</form>
+</div>
+');
+}
+
+add_action( 'admin_menu', 'cbd_admin' );
+add_action( 'admin_head', 'cbd_admin_css' );
+
+function cbd_admin_css() {
+echo('
+<style type="text/css">
+.cbd_admin_hint {
+font-size: 7pt;
+font-style: italic;
+color: #080;
+}
+</style>
+');
+}
+
+function cbd_loader() {
+$cbd_str = get_option( 'cbd_str' );
+$cbd_str_rep = strtr( $cbd_str, array( '&#34;' => '"', '\\' => '', '&#39;' => '' ) );
+echo('
+<div class="cbd">'.$cbd_str_rep.'</div>
+');
+}
+
+add_action( 'get_footer', 'cbd_loader' );
+
+function cbd_loader_css() {
+$cbd_left_right = get_option( 'cbd_left_right' );
+$cbd_top_bottom = get_option( 'cbd_top_bottom' );
+$cbd_left_right_px = get_option( 'cbd_left_right_px' );
+$cbd_top_bottom_px = get_option( 'cbd_top_bottom_px' );
+
+echo('
+<style type="text/css">
+.cbd {
+position: fixed;
+'.$cbd_left_right.': '.$cbd_left_right_px.'px;
+'.$cbd_top_bottom.': '.$cbd_top_bottom_px.'px;
+}
+.cbd a img {
+border: 0;
+}
+</style>
+');
+}
+
+add_action( 'wp_head', 'cbd_loader_css' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Corner Blog Decoration ===
+
+Contributors: Imanuel Novian (noe)
+Donate link: http://www.noeonline.info/
+Tags: corner, blog, decoration
+Requires at least: 2.8
+Tested up to: 3.0.1
+Stable Tag: 3.0.1
+
+This plugin will create an decoration on your blog at the corner of user browser like a blogspot blog.
+
+== Description ==
+
+This plugin will create an decoration on your blog at the corner of user browser like a blogspot blog. You can customize these decoration. Let's check the screenshot for an example. The decoration menu can be found on <a href="tools.php?page=corner-blog-decoration.php">Tools > Blog Decoration</a>.
+
+== Installation ==
+
+1. Upload corner-blog-decoration.php to the /wp-content/plugins/ directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Custom your blog decoration on 'Tools > Blog Decoration' menu.
+
+== Frequently Asked Questions ==
+
+FAQ are not available for right now
+
+== Screenshots ==
+
+1. screenshot-1.png
+2. screenshot-2.png
+3. screenshot-3.png
+
+== Changelog ==
+
+= 1.0 =
+Initial Version
